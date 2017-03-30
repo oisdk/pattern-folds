0ed63851a68809277fc939c336a8d65f5b7f8c48
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-03-30 22:32:02 +0100

    Better classes

--- src/RecordCase.hs
+++ src/RecordCase.hs
@@ -1,59 +1,31 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE KindSignatures        #-}
-{-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE RecordWildCards       #-}
-{-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE DeriveFunctor         #-}
-{-# LANGUAGE RankNTypes     #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module RecordCase
-  (makeHandler
-  ,Handles(..)
-  ,MaybeAlg(..)
+  (MaybeAlg(..)
   ,EitherAlg(..)
-  ,ListAlg(..)
-  ,FoldAlg(..)
   ,BoolAlg(..)
   ,($|))
   where
 
-import RecordCase.TH (makeHandler)
+import           Data.Distributive
+import           RecordCase.TH     (makeHandler)
 
--- | Allows matching on constructors to be applied partially. For instance:
---
--- > tail :: [a] -> [a]
--- > tail = recCase ListAlg {..} where
--- >   nil = []
--- >   cons = const id
-class handler `Handles` patt  where
-    recCase :: handler a -> patt -> a
+import           Data.Functor.Rep
 
 infixr 0 $|
-($|) :: (handler `Handles` patt) => handler a -> patt -> a
-($|) = recCase
+($|) :: Representable handler => handler a -> Rep handler -> a
+($|) = index
+{-# INLINE ($|) #-}
 
 makeHandler ''Maybe
 makeHandler ''Either
 makeHandler ''Bool
-
-data ListAlg a b = ListAlg
-    { nil :: b
-    , cons :: a -> [a] -> b
-    } deriving Functor
-
-instance (a ~ b) => ListAlg b `Handles` [a] where
-    recCase r [] = nil r
-    recCase r (x:xs) = cons r x xs
-    {-# INLINE recCase  #-}
-
-data FoldAlg a b = FoldAlg
-    { func :: a -> b -> b
-    , base :: b
-    }
-
-instance (a ~ b, Foldable f) =>
-         FoldAlg a `Handles` f b where
-    recCase (FoldAlg f b) = foldr f b
-    {-# INLINE recCase #-}
--- src/RecordCase/TH.hs
+++ src/RecordCase/TH.hs
@@ -42,7 +42,6 @@ makeDecFolds dec = case dec of
   NewtypeD     _ ty vars _ con  _ -> next ty vars [con]
   _                             -> fail "makeRecordCase: expected type constructor dec"
   where
-
   next ty args cons =
     makeConsFolds ty args (concatMap normalizeCon cons)
 
@@ -60,133 +59,199 @@ normalizeCon (RecGadtC conNames xs _) =
   [ NCon conName [] Nothing (map (view _3) xs) | conName <- conNames ]
 
 makeConsFolds :: Name -> [TyVarBndr] -> [NCon] -> DecsQ
-makeConsFolds nm ty cons = sequenceA [go, rest, appInst]
-  where
-    go = do
-        rn <- newName "r"
-        let r = PlainTV rn
-        case cons of
-            [x]
-              | length (x ^. nconTypes) == 1 ->
-                  NewtypeD [] (foldGadtName nm) (ty ++ [r]) Nothing <$>
-                  (RecC (foldGadtName nm) <$> traverse (h (VarT rn)) [x]) <*>
-                  pure [ConT (mkName "Functor")]
-            _ ->
-                DataD [] (foldGadtName nm) (ty ++ [r]) Nothing <$>
-                (pure . RecC (foldGadtName nm) <$> traverse (h (VarT rn)) cons) <*>
-                pure [ConT (mkName "Functor")]
-    h r ncon =
-        return
-            ( foldConsName (ncon ^. nconName)
-            , Bang NoSourceUnpackedness NoSourceStrictness
-            , let fnt = funcType (ncon ^. nconTypes) r
-              in maybe
-                     fnt
-                     (\c ->
-                           ForallT (_nconForall ncon) c fnt)
-                     (ncon ^. nconCxt))
-    funcType ts r =
-        foldr
-            (\x xs ->
-                  AppT (AppT ArrowT x) xs)
-            r
-            ts
-    origType = foldl AppT (ConT nm) (convertTVBs ty)
-    newTypeLifted xs = foldl AppT (ConT (foldGadtName nm)) (map VarT xs)
-    rest = do
-        innerVars <- traverse (const (newName "a")) ty
-        InstanceD
-            Nothing
-            (zipWith eq' ty innerVars)
-            (foldl
-                 AppT
-                 (ConT (mkName "Handles"))
-                 [newTypeLifted innerVars, origType]) <$>
-            sequence
-                [ FunD (mkName "recCase") . (: []) <$> instanceFunc
-                , pragInlD (mkName "recCase") Inline FunLike AllPhases]
-    eq' x y = AppT (AppT EqualityT (VarT (bndrName x))) (VarT y)
-    instanceFunc = do
-        expr <- newName "x"
-        alg <- newName "handler"
-        Clause [VarP alg, VarP expr] <$>
-            (NormalB <$> (CaseE (VarE expr) <$> traverse (run alg) cons)) <*>
-            pure []
-    run fnc cn = do
-        vars <- traverse (const (newName "x")) (cn ^. nconTypes)
-        pure $
-            Match
-                (ConP (cn ^. nconName) (map VarP vars))
-                (NormalB
-                     (foldl
-                          AppE
-                          (VarE (foldConsName (cn ^. nconName)))
-                          (map VarE (fnc : vars))))
+makeConsFolds nm ty cons = do
+    newTyName <- pure (foldGadtName nm)
+    let monInst = do
+            lhs <- newName "x"
+            rhs <- newName "f"
+            InstanceD Nothing [] (AppT (ConT (mkName "Monad")) newTypeLifted') <$>
+                sequence
+                    [ FunD (mkName ">>=") <$>
+                      sequence
+                          [ Clause [VarP lhs, VarP rhs] <$>
+                            (NormalB <$> bind lhs rhs) <*>
+                            pure []]
+                    , pragInlD (mkName ">>=") Inline FunLike AllPhases
+                    ]
+        bind lhs rhs =
+            foldl AppE (ConE newTyName) <$> traverse (bind' lhs rhs) cons
+        bind' lhs rhs cn =
+            case length (cn ^. nconTypes) of
+                0 ->
+                    pure $
+                    foldr1
+                        AppE
+                        [ VarE (foldConsName (cn ^. nconName))
+                        , VarE rhs
+                        , VarE (foldConsName (cn ^. nconName))
+                        , VarE lhs]
+                _ -> do
+                    vars <- traverse (const (newName "x")) (cn ^. nconTypes)
+                    let appp l =
+                            foldl
+                                AppE
+                                (VarE (foldConsName (cn ^. nconName)))
+                                (l : map VarE vars)
+                    pure $
+                        LamE
+                            (map VarP vars)
+                            (appp (AppE (VarE rhs) (appp (VarE lhs))))
+        go = do
+            rn <- newName "r"
+            let r = PlainTV rn
+            case cons of
+                [x] ->
+                    NewtypeD [] newTyName (ty ++ [r]) Nothing <$>
+                    (RecC newTyName <$> traverse (h (VarT rn)) [x]) <*>
+                    pure [ConT (mkName "Functor")]
+                _ ->
+                    DataD [] newTyName (ty ++ [r]) Nothing <$>
+                    (pure . RecC newTyName <$> traverse (h (VarT rn)) cons) <*>
+                    pure [ConT (mkName "Functor")]
+        h r ncon =
+            return
+                ( foldConsName (ncon ^. nconName)
+                , Bang NoSourceUnpackedness NoSourceStrictness
+                , let fnt = funcType (ncon ^. nconTypes) r
+                  in maybe
+                         fnt
+                         (\c ->
+                               ForallT (_nconForall ncon) c fnt)
+                         (ncon ^. nconCxt))
+        funcType ts r =
+            foldr
+                (\x xs ->
+                      AppT (AppT ArrowT x) xs)
+                r
+                ts
+        origType = foldl AppT (ConT nm) (convertTVBs ty)
+        newTypeLifted xs = foldl AppT (ConT newTyName) (map VarT xs)
+        newTypeLifted' = foldl AppT (ConT newTyName) (convertTVBs ty)
+        rest =
+            InstanceD
+                Nothing
                 []
-    appInst = do
-        pnm <- newName "x"
-        lhs <- traverse (const (newName "fs")) cons
-        rhs <- traverse (const (newName "xs")) cons
-        InstanceD
-            Nothing
-            []
-            (AppT
-                 (ConT (mkName "Applicative"))
-                 (newTypeLifted (map bndrName ty))) <$>
+                (AppT (ConT (mkName "Representable")) newTypeLifted') <$>
             sequence
                 [ pure $
-                  FunD
-                      (mkName "pure")
-                      [ Clause
-                            [VarP pnm]
-                            (NormalB
+                  TySynInstD
+                      (mkName "Rep")
+                      (TySynEqn [newTypeLifted'] origType)
+                , FunD (mkName "index") . (: []) <$> instanceFunc
+                , pragInlD (mkName "index") Inline FunLike AllPhases
+                , FunD (mkName "tabulate") . (: []) <$> mkHandlerFunc
+                , pragInlD (mkName "tabulate") Inline FunLike AllPhases]
+        distInst =
+            pure $
+            InstanceD
+                Nothing
+                []
+                (AppT (ConT (mkName "Distributive")) newTypeLifted')
+                [ FunD
+                      (mkName "distribute")
+                      [Clause [] (NormalB (VarE (mkName "distributeRep"))) []]]
+        mkHandlerFunc = do
+            alg <- newName "alg"
+            Clause [VarP alg] <$>
+                (NormalB . foldl AppE (ConE newTyName) <$>
+                 traverse (gi alg) cons) <*>
+                pure []
+        gi alg cn = do
+            vars <- traverse (const (newName "x")) (cn ^. nconTypes)
+            pure $
+                case vars of
+                    [] -> AppE (VarE alg) (ConE (cn ^. nconName))
+                    _ ->
+                        LamE
+                            (map VarP vars)
+                            (AppE
+                                 (VarE alg)
                                  (foldl
                                       AppE
-                                      (ConE (foldGadtName nm))
-                                      (map (rep' pnm) cons)))
-                            []]
-                , pragInlD (mkName "pure") Inline FunLike AllPhases
-                , FunD (mkName "<*>") <$>
-                  sequence
-                      [ Clause
-                            [ ConP (foldGadtName nm) (map VarP lhs)
-                            , ConP (foldGadtName nm) (map VarP rhs)] <$>
-                        (NormalB <$>
-                         (foldl AppE (ConE (foldGadtName nm)) <$>
-                          sequence (zipWith3 app' lhs rhs cons))) <*>
-                        pure []]
-                , pragInlD (mkName "<*>") Inline FunLike AllPhases
-                , pure $
-                  FunD
-                      (mkName "<*")
-                      [Clause [] (NormalB (VarE (mkName "const"))) []]
-                , pure $
-                  FunD
-                      (mkName "*>")
-                      [ Clause
-                            []
-                            (NormalB
-                                 (AppE
-                                      (VarE (mkName "const"))
-                                      (VarE (mkName "id"))))
-                            []]
-                , pragInlD (mkName "<*") Inline FunLike AllPhases
-                , pragInlD (mkName "*>") Inline FunLike AllPhases]
-    rep' pnm cns =
-        case length (cns ^. nconTypes) of
-            0 -> VarE pnm
-            n -> LamE (replicate n WildP) (VarE pnm)
-    app' f x c =
-        case length (c ^. nconTypes) of
-            0 -> pure (AppE (VarE f) (VarE x))
-            n -> do
-                vars <- replicateM n (newName "y")
-                pure $
-                    LamE
-                        (map VarP vars)
-                        (AppE
-                             (foldl AppE (VarE f) (map VarE vars))
-                             (foldl AppE (VarE x) (map VarE vars)))
+                                      (ConE (cn ^. nconName))
+                                      (map VarE vars)))
+        instanceFunc = do
+            expr <- newName "x"
+            alg <- newName "handler"
+            Clause [VarP alg, VarP expr] <$>
+                (NormalB <$> (CaseE (VarE expr) <$> traverse (run alg) cons)) <*>
+                pure []
+        run fnc cn = do
+            vars <- traverse (const (newName "x")) (cn ^. nconTypes)
+            pure $
+                Match
+                    (ConP (cn ^. nconName) (map VarP vars))
+                    (NormalB
+                         (foldl
+                              AppE
+                              (VarE (foldConsName (cn ^. nconName)))
+                              (map VarE (fnc : vars))))
+                    []
+        appInst = do
+            pnm <- newName "x"
+            lhs <- traverse (const (newName "fs")) cons
+            rhs <- traverse (const (newName "xs")) cons
+            InstanceD
+                Nothing
+                []
+                (AppT
+                     (ConT (mkName "Applicative"))
+                     (newTypeLifted (map bndrName ty))) <$>
+                sequence
+                    [ pure $
+                      FunD
+                          (mkName "pure")
+                          [ Clause
+                                [VarP pnm]
+                                (NormalB
+                                     (foldl
+                                          AppE
+                                          (ConE newTyName)
+                                          (map (rep' pnm) cons)))
+                                []]
+                    , pragInlD (mkName "pure") Inline FunLike AllPhases
+                    , FunD (mkName "<*>") <$>
+                      sequence
+                          [ Clause
+                                [ ConP newTyName (map VarP lhs)
+                                , ConP newTyName (map VarP rhs)] <$>
+                            (NormalB <$>
+                             (foldl AppE (ConE newTyName) <$>
+                              sequence (zipWith3 app' lhs rhs cons))) <*>
+                            pure []]
+                    , pragInlD (mkName "<*>") Inline FunLike AllPhases
+                    , pure $
+                      FunD
+                          (mkName "<*")
+                          [Clause [] (NormalB (VarE (mkName "const"))) []]
+                    , pure $
+                      FunD
+                          (mkName "*>")
+                          [ Clause
+                                []
+                                (NormalB
+                                     (AppE
+                                          (VarE (mkName "const"))
+                                          (VarE (mkName "id"))))
+                                []]
+                    , pragInlD (mkName "<*") Inline FunLike AllPhases
+                    , pragInlD (mkName "*>") Inline FunLike AllPhases]
+        rep' pnm cns =
+            case length (cns ^. nconTypes) of
+                0 -> VarE pnm
+                n -> LamE (replicate n WildP) (VarE pnm)
+        app' f x c =
+            case length (c ^. nconTypes) of
+                0 -> pure (AppE (VarE f) (VarE x))
+                n -> do
+                    vars <- replicateM n (newName "y")
+                    pure $
+                        LamE
+                            (map VarP vars)
+                            (AppE
+                                 (foldl AppE (VarE f) (map VarE vars))
+                                 (foldl AppE (VarE x) (map VarE vars)))
+    sequenceA [go, rest, appInst, distInst, monInst]
 
 foldGadtName :: Name -> Name
 foldGadtName n = case nameBase n of
